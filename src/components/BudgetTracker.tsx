ad0e6194e6d2103d67e252f9b4e93b6a76e69bc5
Umesh Joshi <slackuj@gmail.com>
2026-02-05 22:43:20 +0545

    commit updates

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -31,12 +31,81 @@ const INITIAL_BUDGET_ITEMS: budgetItem[] = [
 const BudgetTracker = () => {
 
     const [ budgetItems, setBudgetItems] = useState<budgetItem []>(INITIAL_BUDGET_ITEMS);
-    console.log(budgetItems);
+    const [description, setDescription] = useState("");
+    const [amount, setAmount] = useState<number>();
+    const [date, setDate] = useState('');
+    const [category, setCategory] = useState<Category>('Expense');
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if(description.trim().length === 0){
+            alert("Please enter description");
+            return;
+        }
+
+        if (amount === undefined || isNaN(amount) || amount <= 0) {
+            alert("Please enter amount");
+            return;
+        }
+
+        const itemToAdd: budgetItem = {
+            id: crypto.randomUUID(),
+            description,
+            amount: amount ?? 0,
+            date: date,
+            category: category
+        };
+
+        setBudgetItems(prev=> [...prev, itemToAdd]);
+        setDescription("");
+        setAmount(0);
+        setDate("");
+        setCategory("Expense");
+    };
+
+    const handleDelete = (id: string) => {
+        const newBudgetItems = budgetItems.filter(item => item.id !== id);
+        setBudgetItems(newBudgetItems);
+    }
+
     return (
         <div className="budget-tracker">
             <div>
                 <h2>Form</h2>
-                <form></form>
+                <form className="budget-tracker-form" onSubmit={handleSubmit}>
+                   <input
+                       type="text"
+                       placeholder="Enter budget items ..."
+                       value={description}
+                       onChange={(e) => setDescription(e.target.value)}
+                       className="budget-tracker-form__input"
+                   />
+                    <input
+                        type="number"
+                        placeholder="Enter amount"
+                        value={amount}
+                        onChange={(e) => setAmount(Number(e.target.value))}
+                        className="budget-tracker-form__input"
+                    />
+                    <input
+                        type="date"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                        className="budget-tracker-form__input"
+                    />
+                    <select
+                        value={category}
+                        onChange={(e) => {setCategory(e.target.value as Category)}}
+                    >
+                        <option value="Income">Income</option>
+                        <option value="Expense">Expense</option>
+                    </select>
+                    <button
+                        type="submit"
+                        className="budget-tracker-form__submit"
+                    >Submit</button>
+                </form>
             </div>
             <div className="budget-list">
                 {budgetItems.map(item => (
@@ -45,6 +114,7 @@ const BudgetTracker = () => {
                         <p className="b-amount">{item.amount}</p>
                         <p className="b-date">{item.date}</p>
                         <p className="b-category">{item.category}</p>
+                        <button onClick={() => handleDelete(item.id)}>Delete</button>
                     </div>
                 ))}
             </div>
